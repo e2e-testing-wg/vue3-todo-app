1cf7fd769add6886a0cc06ee348dc96f99e64b26
Milan <mmatic@hispatecanalytics.com>
2022-11-12 19:59:29 +0000

    chore(tailwindcss): Add tailwindcss to project

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // eslint-disable-next-line prettier/prettier
+  content: [
+    "./index.html", 
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
